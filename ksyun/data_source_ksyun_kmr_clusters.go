d6271b9975a059358b46a3faa6e2541da6cccf82
zhangrongrong1 <2639735407@qq.com>
2025-11-11 17:29:10 +0800

    terraform

--- ksyun/data_source_ksyun_kmr_clusters.go
+++ ksyun/data_source_ksyun_kmr_clusters.go
@@ -1,3 +1,46 @@
+/*
+Provides a KMR Clusters data source.
+
+# Example Usage
+
+```hcl
+
+	data "ksyun_kmr_clusters" "default" {
+	  marker = "limit=10&offset=0"
+	}
+
+	output "total" {
+	  value = data.ksyun_kmr_clusters.default.total
+	}
+
+```
+
+# Argument Reference
+
+* `marker` - (Optional) Pagination marker, e.g., limit=100&offset=0.
+* `output_file` - (Optional) File name where to save data source results.
+
+# Attributes Reference
+
+* `total` - Total number of KMR clusters.
+* `clusters` - List of KMR clusters.
+  - `cluster_id` - The ID of the cluster.
+  - `cluster_name` - The name of the cluster.
+  - `main_version` - The main version of the cluster.
+  - `enable_eip` - Whether EIP is enabled.
+  - `region` - The region of the cluster.
+  - `vpc_domain_id` - The VPC domain ID.
+  - `charge_type` - The charge type.
+  - `cluster_status` - The status of the cluster.
+  - `create_time` - The creation time.
+  - `update_time` - The update time.
+  - `serving_minutes` - The serving minutes.
+  - `instance_groups` - List of instance groups.
+  - `id` - The ID of the instance group.
+  - `instance_group_type` - The type of the instance group.
+  - `instance_type` - The instance type.
+  - `resource_type` - The resource type.
+*/
 package ksyun
 
 import (
